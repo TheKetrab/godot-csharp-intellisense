612abcd5d4bda296dab73d39b282d29021c0b7cd
Bartłomiej Grochowski <ketrab508@gmail.com>
2021-01-09 20:48:31 +0100

    Assembly Reader init

---
+++ AssemblyReader/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyReader
+{
+    class Program
+    {
+        // args:
+        //
+        // -assdirs [dirs]     - paths to directories to analyze all dlls
+        // -asspaths [paths]   - paths to concrete dlls
+        // -invoker [typename]
+        // -member [membername]  - concrete member of invoker
+        // -any                - all members of invoker
+        //
+        //
+        // output
+        // TYPE result MODIFIERS
+        //
+        //
+        static void Main(string[] args)
+        {
+            // read input
+            string assdirs = "";
+            string asspaths = "";
+            string invoker = "";
+            string member = "";
+            bool any = false;
+
+            for (int i=0; i<args.Length; i++)
+            {
+                if (args[i].Equals("-assdirs"))
+                {
+                    if (i+1 < args.Length)
+                        assdirs = args[++i];
+                }
+                else if (args[i].Equals("-asspaths"))
+                {
+                    if (i+1 < args.Length)
+                        asspaths = args[++i];
+                }
+                else if (args[i].Equals("-invoker"))
+                {
+                    if (i+1 < args.Length)
+                        invoker = args[++i];
+                }
+                else if (args[i].Equals("-member"))
+                {
+                    if (i+1 < args.Length)
+                        member = args[++i];
+                }
+                else if (args[i].Equals("-any"))
+                {
+                    any = true;
+                }
+            }
+
+            Reader reader = new Reader(assdirs,asspaths)
+            {
+                any = any,
+                member = member,
+                invoker = invoker
+            };
+
+            reader.Run();
+
+        }
+    }
+}
---
+++ AssemblyReader/Reader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyReader
+{
+    class Reader
+    {
+        public bool any;
+        public string member;
+        public string invoker;
+
+        public List<Assembly> assemblies;
+
+        public Reader(string assdirs, string asspaths)
+        {
+            // load assemblies from directories0
+            if (!string.IsNullOrEmpty(assdirs))
+            {
+                var splitted = assdirs.Split(' ');
+                foreach (var dir in splitted)
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+                    foreach (var fi in di.GetFiles())
+                        LoadAssembly(fi);
+
+                }                
+            }
+
+            // load directories from files
+            if (!string.IsNullOrEmpty(asspaths))
+            {
+                var splitted = asspaths.Split(' ');
+                foreach (var file in splitted)
+                    LoadAssembly(new FileInfo(file));
+
+            }
+        }
+
+        private bool LoadAssembly(FileInfo fi)
+        {
+            bool success = false;
+            if (fi.Extension.Equals("dll", StringComparison.CurrentCultureIgnoreCase))
+            {
+                try
+                {
+                    Assembly asm = Assembly.LoadFrom(fi.FullName);
+                    assemblies.Add(asm);
+                    success = true;
+                }
+                catch (Exception) { }
+            }
+
+            return success;
+        }
+
+        public void Run()
+        {
+            Type t;
+        }
+
+
+    }
+}
